715c30f5e746ab9b3134f43e3dbaae458e236d32
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-03-29 14:05:32 +1100

    Fixing travis aws setup

--- create_aws_profile.sh
+++ create_aws_profile.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
@@ -6,7 +9,7 @@ aws_access_key_id     = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
 
-cat > ~/.aws/credentials << EOL
+cat > ~/.aws/config << EOL
 [profile packer]
 role_arn       = arn:aws:iam::472057503814:role/ops-admin
 source_profile = default
