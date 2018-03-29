c66b373628591647d8bcc6417b584e4787738c3e
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-03-29 14:01:50 +1100

    Add AWS support to travis

---
+++ create_aws_profile.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials << EOL
+[default]
+aws_access_key_id     = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOL
+
+cat > ~/.aws/credentials << EOL
+[profile packer]
+role_arn       = arn:aws:iam::472057503814:role/ops-admin
+source_profile = default
+region         = ap-southeast-2
+EOL
