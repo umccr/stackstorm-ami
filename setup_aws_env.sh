ecb12fece500db126934cc45ec1ad7566e4ff473
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-03-29 17:05:41 +1100

    Troubleshooting

--- setup_aws_env.sh
+++ setup_aws_env.sh
@@ -9,4 +9,7 @@ export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
 
-env | grep AWS # TODO: test this
+env | grep AWS
+
+packer --version
+packer build stackstorm.json
