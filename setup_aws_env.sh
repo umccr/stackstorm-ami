9a92837142ec7ba37cdb3eb2a8f8861cc2bd6c02
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-03-29 15:17:25 +1100

    Change profile for approach for env modification

---
+++ setup_aws_env.sh
@@ -0,0 +1,7 @@
+export AWS_REGION=ap-southeast-2
+
+temp_role=$(aws sts assume-role --role-arn "arn:aws:iam::472057503814:role/ops_admin_no_mfa" --role-session-name "temp_session")
+
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
