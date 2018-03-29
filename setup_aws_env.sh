77a3c66d1b2449dd76fdb0e7c891098a1d138cee
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-03-29 16:55:16 +1100

    Troubleshooting

--- setup_aws_env.sh
+++ setup_aws_env.sh
@@ -8,3 +8,5 @@ temp_role=$(aws sts assume-role --role-arn "arn:aws:iam::472057503814:role/ops_a
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
+
+env | grep AWS # TODO: test this
