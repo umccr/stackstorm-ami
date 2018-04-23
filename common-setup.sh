93b79b649bf3f5f945a87f9fa8efc4409453e40e
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-04-23 11:51:41 +1000

    Remove rexray and s3fs installation blocks

--- common-setup.sh
+++ common-setup.sh
@@ -33,33 +33,6 @@ sudo apt-get install -y python-pip
 pip install awscli --upgrade
 
 
-# currently not used
-# echo "--------------------------------------------------------------------------------"
-# echo "Installing s3fs"
-# # https://github.com/s3fs-fuse/s3fs-fuse
-# sudo apt-get install -y automake autotools-dev fuse g++ git libcurl4-gnutls-dev libfuse-dev libssl-dev libxml2-dev make pkg-config
-#
-# cd /tmp/
-# git clone https://github.com/s3fs-fuse/s3fs-fuse.git
-# cd s3fs-fuse
-# ./autogen.sh
-# ./configure
-# make
-# sudo make install
-#
-# echo "Configuring s3fs"
-# echo "user_allow_other" | sudo tee -a /etc/fuse.conf
-
-
-
-# echo "--------------------------------------------------------------------------------"
-# echo "Installing rexray"
-# ##### REX-Ray (not needed for the docker plugins)
-# # https://rexray.readthedocs.io/en/stable/user-guide/installation/
-# cd /tmp/
-# curl -sSL https://rexray.io/install | sh -s -- stable
-
-
 echo "--------------------------------------------------------------------------------"
 echo "Adding SSH public keys"
 # Allows *public* members on UMCCR org to SSH to our AMIs
