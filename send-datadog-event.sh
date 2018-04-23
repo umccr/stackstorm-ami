a984edc2040abf1d567de83c042be5a1d4642ce1
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-04-23 12:03:06 +1000

    Increase verbosity

--- send-datadog-event.sh
+++ send-datadog-event.sh
@@ -11,8 +11,8 @@ appkey = ${DATADOG_APP_KEY}
 END
 
 echo "Extracting AMI ID"
-grep 'artifact,0,id' packer-build.log
-
 ami_id=`grep 'artifact,0,id' packer-build.log | cut -d, -f6 | cut -d: -f2`
 
+echo "Extracted AMI ID: $ami_id. Sending DataDog event..."
 dog event post --no_host --tags aws,ami,stackstorm --type travis "New StackStorm AMI created" "$ami_id"
+echo "Event successfully sent."
